491030a4d9c0da17e4001aaa0971f2e3accf7630
Smallsun2025 <lihongyang2025@gmail.com>
2025-06-01 22:38:51 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "East US"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
